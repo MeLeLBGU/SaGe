f0fc20c6b2c27e43dbe3dd98a9b5ead4921721ac
ShakedY <shaked@yehezkel.net>
2023-04-06 19:41:27 +0300

    log 2 -> log

--- Python-Modules/Utils.py
+++ Python-Modules/Utils.py
@@ -42,7 +42,7 @@ def get_not_ablateable_tokens_list(current_vocab):
     return NAT_list
 
 # calculating 'offset' = number of tokens added because of our new encoding
-def calculate_token_offset(i, last_times_accumulate_offset, tokens_in_line, updated_tokens_in_line, log2):
+def calculate_token_offset(i, last_times_accumulate_offset, tokens_in_line, updated_tokens_in_line, log):
     # original encoding next_word_index
     original_current_index = i+1
     if original_current_index == len(tokens_in_line):
@@ -61,8 +61,8 @@ def calculate_token_offset(i, last_times_accumulate_offset, tokens_in_line, upda
         current_index = last_times_accumulate_offset + i
     else:
         if (current_index >= len(updated_tokens_in_line)):
-            log2.info("Fail! current index = {}, length of \"updated_tokens_in_line\" = {}".format(current_index, len(updated_tokens_in_line)))
-            log2.info("token #{}, updated tokens:{}".format(i, updated_tokens_in_line))
+            log.info("Fail! current index = {}, length of \"updated_tokens_in_line\" = {}".format(current_index, len(updated_tokens_in_line)))
+            log.info("token #{}, updated tokens:{}".format(i, updated_tokens_in_line))
             current_index -= 1
         else:
             while not updated_tokens_in_line[current_index].startswith("\u2581"):
@@ -101,7 +101,7 @@ def token_to_line_indices_dictionary(current_vocab, corpus_lines):
 ## Functions for multiprocessing - cannot be class methods #########
 ####################################################################
 
-def sg_for_window_mp(target_token, window, target_embeddings, context_embeddings, log2):
+def sg_for_window_mp(target_token, window, target_embeddings, context_embeddings, log):
     current_p = 0
     for w in window:
         # calculate current value to add
@@ -110,25 +110,24 @@ def sg_for_window_mp(target_token, window, target_embeddings, context_embeddings
             current_p += math.log(sigmoid(dot_product))
         except:
             pass
-            #log2.info("sigmoid error on token {}, when current_p={}".format(target_token, current_p))
 
     return (-1) * current_p
 
 
-def substract_windows_from_sg_mp(i, tokens_in_line_ints, current_sg, target_embeddings, context_embeddings, window_size, log2):
+def substract_windows_from_sg_mp(i, tokens_in_line_ints, current_sg, target_embeddings, context_embeddings, window_size, log):
     # we should substract windows of all tokens in our original window
     sg_wo = current_sg
     _, context_start, context_end = compute_window(i, tokens_in_line_ints, window_size)
 
     for index in range(context_start, context_end):
         window, _, _ = compute_window(index, tokens_in_line_ints, window_size)
-        sg_wo -= sg_for_window_mp(tokens_in_line_ints[index], window, target_embeddings, context_embeddings, log2)
+        sg_wo -= sg_for_window_mp(tokens_in_line_ints[index], window, target_embeddings, context_embeddings, log)
 
     return sg_wo
 
 def add_windows_to_sg_mp(model, updated_i, offset, updated_context_start, \
                         updated_context_end, updated_tokens_in_line, current_sg, \
-                        target_embeddings, context_embeddings, window_size, log2):
+                        target_embeddings, context_embeddings, window_size, log):
     sg_wo = current_sg
     
     # assume it now looks like: old[context_start:i] + [ablated_token_encoding] + old[i+1:context_end]
@@ -136,7 +135,7 @@ def add_windows_to_sg_mp(model, updated_i, offset, updated_context_start, \
     for index in range(updated_context_start, updated_i):
         try:
             window, _, _ = compute_window(index, updated_tokens_in_line, window_size)
-            sg_wo += sg_for_window_mp(updated_tokens_in_line[index], window, target_embeddings, context_embeddings, log2)
+            sg_wo += sg_for_window_mp(updated_tokens_in_line[index], window, target_embeddings, context_embeddings, log)
         except:
             print("index  {}".format(index))
             print("tokens {}".format(updated_tokens_in_line))
@@ -146,7 +145,7 @@ def add_windows_to_sg_mp(model, updated_i, offset, updated_context_start, \
     for index in range(updated_i, updated_i+offset):
         try:
             window, _, _ = compute_window(index, updated_tokens_in_line, window_size)
-            sg_wo += sg_for_window_mp(updated_tokens_in_line[index], window, target_embeddings, context_embeddings, log2)
+            sg_wo += sg_for_window_mp(updated_tokens_in_line[index], window, target_embeddings, context_embeddings, log)
         except:
             print("index  {}".format(index))
             print("tokens {}".format(updated_tokens_in_line))
@@ -157,7 +156,7 @@ def add_windows_to_sg_mp(model, updated_i, offset, updated_context_start, \
         #print("window for token {}".format(tokens_in_line[index]))
         try:
             window, _, _ = compute_window(index, updated_tokens_in_line, window_size)
-            sg_wo += sg_for_window_mp(updated_tokens_in_line[index], window, target_embeddings, context_embeddings, log2)
+            sg_wo += sg_for_window_mp(updated_tokens_in_line[index], window, target_embeddings, context_embeddings, log)
         except:
             print("index  {}, len {}".format(index, len(updated_tokens_in_line)))
             updated_tokens_in_line_str = [model.id_to_piece(x) for x in updated_tokens_in_line]
@@ -170,7 +169,7 @@ def update_sg_per_instance_of_token_mp(model, token_to_ablate, i, \
                                     line, tokens_in_line_ints, tokens_in_line_pieces, \
                                     last_times_accumulate_offset, \
                                     current_total_sg, current_vocab, \
-                                    target_embeddings, context_embeddings, log2, window_size):
+                                    target_embeddings, context_embeddings, log, window_size):
     
     # window of token - before ablation
     sg_wo = current_total_sg
@@ -178,7 +177,7 @@ def update_sg_per_instance_of_token_mp(model, token_to_ablate, i, \
     # we should substract windows of all tokens in our original window
     original_window, original_context_start, original_context_end = compute_window(i, tokens_in_line_pieces, window_size)
 
-    sg_wo = substract_windows_from_sg_mp(i, tokens_in_line_ints, sg_wo, target_embeddings, context_embeddings, window_size, log2)
+    sg_wo = substract_windows_from_sg_mp(i, tokens_in_line_ints, sg_wo, target_embeddings, context_embeddings, window_size, log)
 
     ## remove token from current vocab
     vocab_without_token = current_vocab.copy()
@@ -190,7 +189,7 @@ def update_sg_per_instance_of_token_mp(model, token_to_ablate, i, \
     updated_tokens_in_line = [model.id_to_piece(u) for u in updated_tokens_in_line_ints]
 
     # calculating 'offset' = number of tokens added because of our new encoding
-    offset = calculate_token_offset(i, last_times_accumulate_offset, tokens_in_line_pieces, updated_tokens_in_line, log2)
+    offset = calculate_token_offset(i, last_times_accumulate_offset, tokens_in_line_pieces, updated_tokens_in_line, log)
 
     #################################################################################
     # make sure the window (before the ablated token) stays the same ################
@@ -203,15 +202,6 @@ def update_sg_per_instance_of_token_mp(model, token_to_ablate, i, \
     first_part_of_window_length = min(window_size, updated_i)
 
     if updated_window[:first_part_of_window_length] != original_window[:first_part_of_window_length]:
-        # if we have former instance of the token in the window of current instance, ignore this computation
-        #if not token_to_ablate in original_window:
-            #''' add this (first log line) back !!!! '''
-            #log2.info("---- ignored: not equal - token {}, original index {}, updated {} ----".format(token_to_ablate, i, updated_i))
-            #self._log2.info("updated_window  {}+{}, (offset {}, last_times {})".format(updated_tokens_in_line[updated_context_start:updated_i], updated_tokens_in_line[updated_i+offset+1:updated_context_end], offset, last_times_accumulate_offset))
-            #self._log2.info("original_window {}".format(original_window))
-            #self._log2.info("updated tokens - {}".format(updated_tokens_in_line))
-            #self._log2.info("original tokens- {}".format(tokens_in_line))
-        # else : this is because some encoding of tokens before our token to ablate changed. we ignore this case too
         return current_total_sg, offset
 
     #################################################################################
@@ -220,7 +210,7 @@ def update_sg_per_instance_of_token_mp(model, token_to_ablate, i, \
     # and add the updated windows sg
     sg_wo = add_windows_to_sg_mp(model, updated_i, offset, updated_context_start, \
                                 updated_context_end, updated_tokens_in_line_ints, \
-                                sg_wo, target_embeddings, context_embeddings, window_size, log2)
+                                sg_wo, target_embeddings, context_embeddings, window_size, log)
 
     ## revert model to current vocab
     model.set_vocabulary(current_vocab)
@@ -230,7 +220,7 @@ def update_sg_per_instance_of_token_mp(model, token_to_ablate, i, \
 ## We assume the line contains the token_to_ablate (check it before shooting new process to execute this method)
 def get_diff_sg_wo_token_for_line(model, line_index, \
                                     corpus_lines, model_encoded_corpus_lines_token_ids, model_encoded_corpus_lines_token_pieces, \
-                                    token_to_ablate, current_vocab, target_embeddings, context_embeddings, log2, window_size):
+                                    token_to_ablate, current_vocab, target_embeddings, context_embeddings, log, window_size):
     sg_wo_diff = 0
 
     tokens_in_line_ints = model_encoded_corpus_lines_token_ids[line_index]
@@ -244,12 +234,12 @@ def get_diff_sg_wo_token_for_line(model, line_index, \
     last_times_accumulate_offset = 0
     for i in indices:
         #print("calling update for instance #{} of {}".format(i, token_to_ablate))
-        sg_wo_diff, last_time_offset = update_sg_per_instance_of_token_mp(model, token_to_ablate, i, line, tokens_in_line_ints, tokens_in_line_pieces, last_times_accumulate_offset, sg_wo_diff, current_vocab, target_embeddings, context_embeddings, log2, window_size)
+        sg_wo_diff, last_time_offset = update_sg_per_instance_of_token_mp(model, token_to_ablate, i, line, tokens_in_line_ints, tokens_in_line_pieces, last_times_accumulate_offset, sg_wo_diff, current_vocab, target_embeddings, context_embeddings, log, window_size)
         last_times_accumulate_offset += last_time_offset
 
     return token_to_ablate, sg_wo_diff
 
-def sg_wo_token_mp(model, token_to_ablate, current_total_sg, current_vocab, training_filepath, target_embeddings, context_embeddings, log2, corpus_lines, window_size):
+def sg_wo_token_mp(model, token_to_ablate, current_total_sg, current_vocab, training_filepath, target_embeddings, context_embeddings, log, corpus_lines, window_size):
     sg_wo = current_total_sg
 
     for line in corpus_lines:
@@ -269,7 +259,7 @@ def sg_wo_token_mp(model, token_to_ablate, current_total_sg, current_vocab, trai
         for i in indices:
             #print("calling update for instance #{} of {}".format(i, token_to_ablate))
             tokens_in_line_ints = [model.piece_to_id(t) for t in tokens_in_line_pieces]
-            sg_wo, last_time_offset = update_sg_per_instance_of_token_mp(model, token_to_ablate, i, line, tokens_in_line_ints, tokens_in_line_pieces, last_times_accumulate_offset, sg_wo, current_vocab, target_embeddings, context_embeddings, log2, window_size)
+            sg_wo, last_time_offset = update_sg_per_instance_of_token_mp(model, token_to_ablate, i, line, tokens_in_line_ints, tokens_in_line_pieces, last_times_accumulate_offset, sg_wo, current_vocab, target_embeddings, context_embeddings, log, window_size)
             last_times_accumulate_offset += last_time_offset
 
     return token_to_ablate, sg_wo
