b8ad1625d075017720cacf7e0b84712556a3f961
Yuval Pinter <yuvalpinter@users.noreply.github.com>
2023-11-07 20:40:35 +0200

    remove ids

--- fast_sage/run.sh
+++ fast_sage/run.sh
@@ -1,4 +1,4 @@
 python fast_sage_main.py test40k \
-    --corpus_filepath=/Users/craigschmidt/kensho/src/kensho-tokenization/data/minipile_10k.txt \
-    --vocab_filepath=/Users/craigschmidt/kensho/src/kensho-tokenization/data/orig/vocabulary_16.vocab \
+    --corpus_filepath=../data/minipile_10k.txt \
+    --vocab_filepath=../data/orig/vocabulary_16.vocab \
     --max_len=16
